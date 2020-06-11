9c3344b7e16d80f0bced6b8d0c54f8989a19a6d3
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-11 13:49:05 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def name=(puppy)
+    @dogs_name = puppy
+  end
+  
+  def name
+    @dogs_name
+  end
+  
+  def breed=(puppy_breed)
+    @dogs_breed = puppy_breed
+  end
+  
+  def breed
+    @dogs_breed
+  end
+end
